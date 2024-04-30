a0b8ebc2dafc422cc68498449eea152b8f64339a
alisatresnjic <atresnjic@schueler.hakzell.at>
2024-04-30 12:09:21 +0200

    remove .idea ordner

---
+++ src/Day.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Alisatresnjic\Weather;
+
+class Day
+{
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alisatresnjic/weather',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9cddbd6e652a85a0245bc53c0a325c0ade9f637e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alisatresnjic/weather' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9cddbd6e652a85a0245bc53c0a325c0ade9f637e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
